eb43a7bc562b1d153426293e68e23d4314d46398
mikechess <mikecook450@gmail.com>
2024-01-13 17:05:08 -0500

    02.Creating HomePage and components and layout fonts and global.css

---
+++ app/components/TopPokemon.jsx
@@ -0,0 +1,9 @@
+
+
+const TopPokemon = () => {
+  return (
+    <div>TopPokemon</div>
+  )
+}
+
+export default TopPokemon
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,18 @@
-import { Inter } from 'next/font/google'
+import { Bangers, Lato } from 'next/font/google'
 import './globals.css'
+import Header from './components/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const bangers = Bangers({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bangers",
+});
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-lato",
+});
 
 export const metadata = {
   title: 'Pokemon Profiler',
@@ -11,7 +22,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${bangers.variable} ${lato.variable}`}>
+        <Header />
         <main className='container py-12'>
           {children}
         </main>
