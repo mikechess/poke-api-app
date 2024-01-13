78ffa22b2e389f9739bb7b623949d7e2a87f0c84
mikechess <mikecook450@gmail.com>
2024-01-13 17:25:19 -0500

    04.Building a  Header component

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,7 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="navbar">
+        <Image
+          height={200}
+          width={200}
+          src="/pokemon-logo.png"
+          priority={true}
+          alt="logo"
+        />
+        <small className="text-sm">&trade;&nbsp;</small>
+        <span className="font-bangers">Profiler</span>
+      </div>
+    </header>
   )
 }
 
