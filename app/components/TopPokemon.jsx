51356a897a2b36e7efc4844dd0632273376ce938
mikechess <mikecook450@gmail.com>
2024-01-24 17:15:27 -0500

    06. Building the TopPokemon server component and Making async API calls in server components (GET requests)

--- app/components/TopPokemon.jsx
+++ app/components/TopPokemon.jsx
@@ -1,8 +1,49 @@
+import { BASE_URI, beautify } from '@/utils/helpers';
+import Image from 'next/image';
 
+// Server component API call loads data on page load
+async function fetchPokemonList() {
+  const totalPokemonCount = 1292;
+  const offset = Math.floor(Math.random() * totalPokemonCount) + 1;
+
+  const limit = 10;
+
+  try {
+    const response = await fetch(`${BASE_URI}/pokemon?offset=${offset}&limit=${limit}`, {
+      next: {
+        revalidate: 60,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  return await response.json();
+}
+
+async function fetchPokemonData(pokemonList) {
+  const promise = pokemonList?.map(async (pokemon) => {
+    const response = await fetch(
+      pokemon.url,
+      {
+        next: {
+          revalidate: 60,
+        },
+      }
+    );
+    const data = await response.json();
+
+    return data;
+  });
+
+  return await response.json();
+}
 
 const TopPokemon = () => {
   return (
-    <div>TopPokemon</div>
+    <>
+      <h1 className='font-bangers text-center'>Top Pokémon</h1>
+    </>
   )
 }
 
